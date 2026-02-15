2ed48edd278fcecf48a6156e87633160b6de5b87
OnNorveg <myway2984@gmail.com>
2026-02-15 17:06:33 +0100

    Step 3

--- Task Management System/task/src/taskmanagement/controller/TaskManagementController.java
+++ Task Management System/task/src/taskmanagement/controller/TaskManagementController.java
@@ -3,17 +3,27 @@ package taskmanagement.controller;
 import jakarta.validation.Valid;
 import org.hibernate.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.*;
 import taskmanagement.entity.UserProfile;
 import taskmanagement.service.TaskManagementService;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @RestController
 public class TaskManagementController {
 
     private final TaskManagementService taskManagementService;
+    private final JwtEncoder jwtEncoder;
 
-    public TaskManagementController(TaskManagementService taskManagementService) {
+    public TaskManagementController(TaskManagementService taskManagementService,  JwtEncoder jwtEncoder) {
         this.taskManagementService = taskManagementService;
+        this.jwtEncoder = jwtEncoder;
     }
 
     @PostMapping("/api/accounts")
@@ -29,5 +39,19 @@ public class TaskManagementController {
     @GetMapping("/api/tasks")
     ResponseEntity<?> GetTasks(@RequestParam(name = "author", required = false)  String author) {
         return taskManagementService.getAllTasks(author);
+    }
+
+    @PostMapping("/api/auth/token")
+    String token(Authentication authentication) {
+        var authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority);
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .subject(authentication.getName())
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plus(60, ChronoUnit.SECONDS))
+                .claim("scope", authorities)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
     }
 }
--- Task Management System/task/src/taskmanagement/security/SecurityConfig.java
+++ Task Management System/task/src/taskmanagement/security/SecurityConfig.java
@@ -1,16 +1,26 @@
 package taskmanagement.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration
 public class SecurityConfig {
@@ -22,15 +32,37 @@ public class SecurityConfig {
     }
 
     @Bean
+    public JwtDecoder jwtDecoder(KeyPair  keyPair) {
+        return NimbusJwtDecoder.withPublicKey((RSAPublicKey)keyPair.getPublic()).build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(KeyPair keyPair) {
+        var rsaKey = new RSAKey.Builder((RSAPublicKey)keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        var jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet(jwkSet);
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .httpBasic(Customizer.withDefaults()) // enable basic HTTP authentication
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
                 .authorizeHttpRequests(auth -> auth
                         // other matchers
                         .requestMatchers("/error").permitAll() // expose the /error endpoint
                         .requestMatchers("/actuator/shutdown").permitAll() // required for tests
                         .requestMatchers("/h2-console/**").permitAll() // expose H2 console
                         .requestMatchers("/api/accounts").permitAll()
+                        .requestMatchers("/api/auth/token").authenticated()
                         .requestMatchers("/api/tasks").authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable) // allow modifying requests from tests
